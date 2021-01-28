da304dbf9dd84e05913eb9e9e23d63ccfc49e4f0
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-01-28 22:58:54 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+        Hi,
+        My name is Pankaj Kumar Mandal
+        I am good at C, Python, React js( HTML, CSS, JavaScript,) 
+        Bootstrap, And as a Fresher seeking for an opportunity to work 
+        in a challenging environment to prove my skills and utilize my
+        knowledge and intelligence in the growth of the organization
+        </p>
+
+        {/* <p className="lead">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque rerum
+          hic ab veniam reiciendis cum repudiandae, voluptate explicabo nesciunt
+          nam accusantium? Soluta cupiditate, accusamus commodi praesentium
+          laborum dolorum libero maiores!
+        </p> */}
+        {/* <p className="lead">Lorem ipsum, dolor sit amet consectetur adipisicing 
+        elit. Cumque rerum hic ab veniam reiciendis cum repudiandae, voluptate 
+        explicabo nesciunt nam accusantium? Soluta cupiditate, accusamus commodi
+         praesentium laborum dolorum libero maiores!</p> */}
+      </div>
+    </div>
+  );
+};
+
+export default About;
