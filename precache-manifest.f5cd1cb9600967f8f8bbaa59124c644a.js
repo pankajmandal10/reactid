806d85cf892cb6ce8b18e7a3a5c5896a3c511d41
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-01-29 11:56:58 +0530

    Updates

---
+++ precache-manifest.f5cd1cb9600967f8f8bbaa59124c644a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cd10586e843b13693f62b3b1551234",
+    "url": "/reactid/index.html"
+  },
+  {
+    "revision": "f010af5de9913b002f47",
+    "url": "/reactid/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "e653d75fb346e4939654",
+    "url": "/reactid/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "f010af5de9913b002f47",
+    "url": "/reactid/static/js/2.372b05ee.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reactid/static/js/2.372b05ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e653d75fb346e4939654",
+    "url": "/reactid/static/js/main.8067c0c7.chunk.js"
+  },
+  {
+    "revision": "53bc782958960731e41e",
+    "url": "/reactid/static/js/runtime-main.de29b42f.js"
+  }
+]);
